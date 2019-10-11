a76a750ad581cafa51cf7c3aa0d1efa1c1ef43f1
Tran Hiep <trantronghiep140998@gmail.com>
2019-10-11 15:10:30 +0700

    lan 2

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var port=process.env.PORT || 5000;
 let routes = require('./api/routes') //importing route
 routes(app)
 
@@ -8,6 +8,6 @@ app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 })
 
-app.listen(5000, function () {
+app.listen(port, function () {
   console.log('Server is running....');
 });

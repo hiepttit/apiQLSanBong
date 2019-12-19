95b647f546ebd256f4fcfcaff1025effe7bfd5b2
Tran Hiep <trantronghiep140998@gmail.com>
2019-12-19 16:52:31 +0700

    update lan2

--- api/Dbconnection.js
+++ api/Dbconnection.js
@@ -1,8 +1,8 @@
 var mysql=require('mysql');
 var connection=mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "our_team"
+    host: "remotemysql.com",
+    user: "5Eh2gENuKH",
+    password: "nwuwQm9I9I",
+    database: "5Eh2gENuKH"
 });
 module.exports=connection;
